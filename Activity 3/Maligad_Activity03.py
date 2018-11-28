c313c085fadaf831e63d33901f7a9c145f3005a6
Jam Maligad <31820961+jammmeh@users.noreply.github.com>
2018-11-28 19:37:38 +0800

    updated w desc

--- Activity 3/Maligad_Activity03.py
+++ Activity 3/Maligad_Activity03.py
@@ -1,6 +1,6 @@
 # Maligad, Juan Alphonso D.
 # 11/28/18
-# Activity 3 - Strings
+# Activity 3 - Python script that will accept two string inputs separated by a whitespace containing alphanumeric characters. Create a function that would extract all alpha (a-z, A-Z) characters from the two inputs and store it as a single string. The function should also extract all numbers from both inputs and add them.
 
 def extract(inx, iny):
     a = ""
